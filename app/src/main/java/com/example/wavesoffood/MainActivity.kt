dacaf4938654739c21fa7ff1442e4add7529e8fb
ShayanAKhun <littelmater391@gmail.com>
2024-04-22 20:54:56 +0500

    made a few activities

---
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.wavesoffood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Your Code
+        }, 3000)
+    }
+}
