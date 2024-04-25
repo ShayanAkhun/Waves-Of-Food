20298bebc229d0afb15475f08faed587c643c070
ShayanAKhun <littelmater391@gmail.com>
2024-04-25 22:18:52 +0500

    added navigation from login activity to signin activity

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -1,15 +1,30 @@
 package com.example.wavesoffood
 
 import android.os.Bundle
+import android.content.Intent
+import android.renderscript.ScriptGroup.Binding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
- class LoginActivity : AppCompatActivity() {
+import com.example.wavesoffood.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+     private val binding:ActivityLoginBinding by lazy {
+         ActivityLoginBinding.inflate(layoutInflater)
+     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.loginbutton.setOnClickListener{
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
+        binding.dhabutton.setOnClickListener{
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
