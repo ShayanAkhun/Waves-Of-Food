11199b83777a640725b4d79dd7b280379004eea2
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-08-29 14:47:27 +0500

    recent changes to the app

--- app/src/main/java/com/example/wavesoffood/adapter/popularAdapter.kt
+++ app/src/main/java/com/example/wavesoffood/adapter/popularAdapter.kt
@@ -4,7 +4,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wavesoffood.databinding.PopularItemBinding
 
-class popularAdapter(private val items: List<String>, private val image: List<String>, private val price: List<Int>) : RecyclerView.Adapter<popularAdapter.PopularViewHolder>() {
+class PopularAdapter(private val items: List<String>, private val price: List<String>, private val image: List<Int>) : RecyclerView.Adapter<PopularAdapter.PopularViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
@@ -17,17 +17,18 @@ class popularAdapter(private val items: List<String>, private val image: List<St
         val item = items[position]
         val images = image[position]
         val price = price[position]
-        holder.bind(item, images,price)
+
+        holder.bind(item,price, images)
     }
     override fun getItemCount(): Int {
             return items.size
     }
 
     class PopularViewHolder (private  val binding: PopularItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val imagesView = binding.imageView7
-        fun bind(item: String, images: Int, price: String) {
+        private val imagesView = binding.PopularImages
+        fun bind(item: String,  price: String,images: Int,) {
         binding.foodnamepopular.text = item
-            binding.price.text = price
+            binding.pricePopular.text = price
             imagesView.setImageResource(images)
 
 
--- app/src/main/java/com/example/wavesoffood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/HomeFragment.kt
@@ -6,11 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.wavesoffood.R
-import com.example.wavesoffood.adapter.popularAdapter
+import com.example.wavesoffood.adapter.PopularAdapter
 import com.example.wavesoffood.databinding.FragmentHomeBinding
 
 
@@ -54,11 +55,13 @@ class HomeFragment : Fragment() {
         }
     })
 
-        val foodName = listOf("Herbal Pancake","Fruit Salad", "Green Noodles","item" )
-        val foodPrices = listOf("$5", "$10", "$20")
-        val popularFoodImages = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3)
+        val foodName = listOf("Herbal Pancake","Fruit Salad", "Green Noodles","Chicken Biryani","Burger", "Pizza")
+        val foodPrices = listOf("$5", "$10", "$20","$5", "$10", "$20")
+        val popularFoodImages = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu1,R.drawable.menu2,R.drawable.menu3)
 
-        val adapter = popularAdapter(foodName,foodPrices,popularFoodImages)
+        val adapter = PopularAdapter(foodName,foodPrices,popularFoodImages)
+        binding.PopularRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.PopularRecyclerView.adapter = adapter
 
     }
 
