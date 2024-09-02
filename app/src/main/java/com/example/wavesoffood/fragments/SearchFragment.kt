556bd32e403e89d3a369485db86e154510e21f45
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-09-02 21:55:32 +0500

    search feature

--- app/src/main/java/com/example/wavesoffood/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/SearchFragment.kt
@@ -6,10 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wavesoffood.R
-
+import com.example.wavesoffood.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +24,8 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
