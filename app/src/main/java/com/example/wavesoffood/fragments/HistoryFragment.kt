9de940a0dc48df3f9fc678cad1d8ab98c106d4aa
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-09-05 14:41:59 +0500

    recent buys completed

--- app/src/main/java/com/example/wavesoffood/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/HistoryFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wavesoffood.R
+import com.example.wavesoffood.adapter.BuyAgainAdapter
 import com.example.wavesoffood.databinding.FragmentHistoryBinding
 
 
@@ -16,6 +18,7 @@ import com.example.wavesoffood.databinding.FragmentHistoryBinding
  */
 class HistoryFragment : Fragment() {
         private lateinit var binding: FragmentHistoryBinding
+        private lateinit var buyAgainAdapter: BuyAgainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -28,7 +31,20 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
             binding= FragmentHistoryBinding.inflate(layoutInflater,container,false)
+        setUpRecyclerView()
         return binding.root
     }
+    private fun setUpRecyclerView () {
+        val buyAgainFoodName = arrayListOf("Food 1", "Food 2", "Food 3")
+        val buyAgainFoodPrice = arrayListOf("$5", "$10", "$20")
+        val buyAgainFoodImage = arrayListOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,)
+        buyAgainAdapter = BuyAgainAdapter(buyAgainFoodName,buyAgainFoodPrice,buyAgainFoodImage)
+        binding.buyAgainRecyclerView.adapter = buyAgainAdapter
+        binding.buyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+
+
+
 
 }
